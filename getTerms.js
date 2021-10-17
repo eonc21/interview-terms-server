167f22e7d2f0ba8be10a40e185f727c7238d2a8e
eonc21 <e.onc@student.tudelft.nl>
2021-10-17 19:33:31 +0200

    uhu

--- getTerms.js
+++ getTerms.js
@@ -2,43 +2,78 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require("fs");
+
+const readTextFile = () => {
+    let terms = fs.readFileSync("./terms.txt", async function(err, data) {
+        if (err) throw err
+        let splitLine = []
+    
+        let term = {}
+        let splitted = data.toString().split("\n")
+    
+        for (let i = 0; i < splitted.length; i++) {
+            splitLine = splitted[i].split(", ")
+            term = [splitLine[0], splitLine[1], splitLine[2]]
+        }
+        console.log(term)
+    
+    })
+
+    return terms
+}
+
+// console.log(terms)
+// const mysql = require('mysql')
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'dbuser',
+//     password: 's3kreee7',
+//     database: 'my_db'
+//   })
+
+// connection.connect()
+
 
 const port = 3000;
-let terms = [
-    {
-        category: "OOP",
-        term: "Inheritance",
-        explanation: "This is what it is"
-    }, 
-    {
-        category: "Hardware",
-        term: "Transistor",
-        explanation: "This is what it is"
-    }, 
-    {
-        category: "OOP",
-        term: "Polymorphism",
-        explanation: "This is what it is"
-    },
-]
+const baseUrl = "/interview-prep/api/terms"
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+let terms = []
+
+/**
+ * GET request for terminology
+ * for all the terms
+ */
+ app.get(baseUrl, (req, res) => {
+    fs.readFile("terms.txt", async function(err, data) {
+        if (err) throw err
+        let splitLine = []
+    
+        let term = {}
+        let splitted = data.toString().split("\n")
+        console.log("AAAAAAAAAAAAA ", splitted, `\n`)
+    
+        for (let i = 0; i < splitted.length; i++) {
+            splitLine = splitted[i].split(", ")
+            terms.push([splitLine[0], splitLine[1], splitLine[2]])
+        }
+        console.log(term)
+    
+    })
+    res.json(terms);
+});
 
 
 /**
  * GET request for terminology
  * in a specific category
  */
-
- app.get('/terms', (req, res) => {
-
-    res.json(terms);
-});
-
-
-app.get('/terms/:category', (req, res) => {
+app.get(`${baseUrl}/:category`, (req, res) => {
     let termArray = [];
     const category = req.params.category;
     console.log(category)
@@ -46,15 +81,19 @@ app.get('/terms/:category', (req, res) => {
     terms.filter(term => term.category == category)
             .map(term => termArray.push(term))
 
-
-    // for (let term of terms) {
-    //     if (term.category === category) {
-    //         termArray.push(term)
-    //     }
-    // }
     res.json(termArray)
 
 });
+
+app.post(`${baseUrl}/:category/:term/:explanation`, (req, res) => {
+    const term = req.params.term
+    const category = req.params.category
+    const explanation = req.params.explanation
+
+    const newTerm = {category, term, explanation}
+    terms.push(newTerm)
+    res.send(newTerm)
+})
 
 
 
